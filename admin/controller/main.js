8b19bff40a44c3cad81a414c75c42b96794d7136
LamPhuongZ <lamphuong024@gmail.com>
2023-02-20 02:09:05 +0700

    update UI admin

---
+++ admin/controller/main.js
@@ -0,0 +1,40 @@
+getProducts();
+
+// Hàm gửi yêu cầu lấy danh sách sản phẩm từ API
+function getProducts() {
+    axios({
+        method: "GET",
+        url: "https://63f11fda5703e063fa532dfb.mockapi.io/api/products"
+    })
+        .then((response) => {
+            // Call API thành công
+            renderProducts(response.data);
+        })
+}
+
+// Hàm hiển thị danh sách sản phẩm ra table
+function renderProducts(products) {
+    let html = products.reduce((result, product, index) => {
+        return result + `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${product.name}</td>
+                <td>${Intl.NumberFormat('vn-VN').format(product.price)}</td>
+                <td>
+                    <img src="${product.img}" width="100" height="100" /> 
+                </td>
+                <td>${product.desc}</td>
+                <td> 
+                    <button type="button" class="btn btn-secondary">Edit</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        `
+    }, "");
+    getElement("#tblListProducts").innerHTML = html;
+};
+
+
+function getElement(selector) {
+    return document.querySelector(selector);
+};
