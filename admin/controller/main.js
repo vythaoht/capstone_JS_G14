46c5e96d76553bcfc11725f86da2939635c214e6
Vy <motchiecvee@Little-Vee.local>
2023-02-21 12:40:29 +0700

    chuc nang delete cau 1.3

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -2,39 +2,53 @@ getProducts();
 
 // Hàm gửi yêu cầu lấy danh sách sản phẩm từ API
 function getProducts() {
-    axios({
-        method: "GET",
-        url: "https://63f11fda5703e063fa532dfb.mockapi.io/api/products"
-    })
-        .then((response) => {
-            // Call API thành công
-            renderProducts(response.data);
-        })
+  axios({
+    method: "GET",
+    url: "https://63f11fda5703e063fa532dfb.mockapi.io/api/products",
+  }).then((response) => {
+    // Call API thành công
+    renderProducts(response.data);
+  });
 }
 
 // Hàm hiển thị danh sách sản phẩm ra table
 function renderProducts(products) {
-    let html = products.reduce((result, product, index) => {
-        return result + `
+  let html = products.reduce((result, product, index) => {
+    return (
+      result +
+      `
             <tr>
                 <td>${index + 1}</td>
                 <td>${product.name}</td>
-                <td>${Intl.NumberFormat('vn-VN').format(product.price)}</td>
+                <td>${Intl.NumberFormat("vn-VN").format(product.price)}</td>
                 <td>
                     <img src="${product.img}" width="100" height="100" /> 
                 </td>
                 <td>${product.desc}</td>
                 <td> 
                     <button type="button" class="btn btn-secondary">Edit</button>
-                    <button type="button" class="btn btn-danger">Delete</button>
+                    <button type="button" class="btn btn-danger" onclick="deleteProduct(${
+                      product.id
+                    })">Delete</button>
                 </td>
             </tr>
         `
-    }, "");
-    getElement("#tblListProducts").innerHTML = html;
-};
+    );
+  }, "");
+  getElement("#tblListProducts").innerHTML = html;
+}
 
+//Ham xoa san pham
+function deleteProduct(productId) {
+  apiDeleteProduct(productId)
+    .then(() => {
+      getProducts();
+    })
+    .catch(() => {
+      alert("Something wrong!");
+    });
+}
 
 function getElement(selector) {
-    return document.querySelector(selector);
-};
+  return document.querySelector(selector);
+}
